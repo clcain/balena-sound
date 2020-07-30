a5030ad50a546f74c7e6ac63eae2fd1d6ec2d33b
Chandler Cain <chandler@clcain.com>
2020-07-30 00:56:51 -0400

    Add static SNAPCAST_SERVER env variable

--- fleet-supervisor/start.sh
+++ fleet-supervisor/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start fleet supervisor if multi room is enabled
-if [[ -z $DISABLE_MULTI_ROOM ]]; then
+if [[ -z $DISABLE_MULTI_ROOM && -z $SNAPCAST_SERVER ]]; then
   # Wait for resin_supervisor to start up
   # We need this because depends_on: resin_supervisor is not supported
   while ! curl -s -X GET --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/ping?apikey=$BALENA_SUPERVISOR_API_KEY"; do sleep 1; done
--- snapcast-client/start.sh
+++ snapcast-client/start.sh
@@ -4,7 +4,9 @@
 if [[ -z $DISABLE_MULTI_ROOM ]]; then
   # Wait for fleet-supervisor to start up
   # We need this because fleet-supervisor depends on resin_supervisor, which has no support for depends_on
-  while ! curl -s "http://localhost:3000"; do sleep 1; done
+  if [[ -z $SNAPCAST_SERVER ]]; then
+    while ! curl -s "http://localhost:3000"; do sleep 1; done
+  fi
 
   # Add latency if defined to compensate for speaker hardware sync issues
   if [[ -n $DEVICE_LATENCY ]]; then
@@ -12,7 +14,9 @@ if [[ -z $DISABLE_MULTI_ROOM ]]; then
   fi
 
   # Start snapclient
-  SNAPCAST_SERVER=$(curl --silent http://localhost:3000)
+  if [[ -z $SNAPCAST_SERVER ]]; then
+    SNAPCAST_SERVER=$(curl --silent http://localhost:3000)
+  fi
   echo -e "Starting snapclient...\nTarget snapcast server: $SNAPCAST_SERVER"
   /usr/bin/snapclient -h $SNAPCAST_SERVER $LATENCY
 else
