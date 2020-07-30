6040b1c4e03422d3d01d3c0c351f4f644f00d1b6
Chandler Cain <chandler@clcain.com>
2020-07-30 03:18:07 -0400

    Allow airplay to run locally when CLIENT_ONLY_MULTI_ROOM is enabled

--- airplay/start.sh
+++ airplay/start.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-#Check for incompatible multi room and client-only setting
-if [[ -n $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
-  echo “DISABLE_MULTI_ROOM and CLIENT_ONLY_MULTI_ROOM cannot be set simultaneously. Ignoring client-only mode.”
-fi
-
-#Exit service if client-only mode is enabled
-if [[ -z $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
-  exit 0
-fi
-
-
 # Set the device broadcast name for AirPlay
 if [[ -z "$DEVICE_NAME" ]]; then
   DEVICE_NAME=$(printf "balenaSound Airplay %s" $(hostname | cut -c -4))
@@ -18,7 +7,7 @@ fi
 
 # Use pipe output if multi room is enabled
 # Don't pipe for Pi 1/2 family devices since snapcast-server is disabled by default
-if [[ -z $DISABLE_MULTI_ROOM ]] && ! [[ $BALENA_DEVICE_TYPE == "raspberry-pi" || $BALENA_DEVICE_TYPE == "raspberry-pi2" ]]; then
+if [[ -z $DISABLE_MULTI_ROOM ]] && [[ -z $CLIENT_ONLY_MULTI_ROOM ]] && ! [[ $BALENA_DEVICE_TYPE == "raspberry-pi" || $BALENA_DEVICE_TYPE == "raspberry-pi2" ]]; then
   SHAIRPORT_BACKEND="-o pipe -- /var/cache/snapcast/snapfifo"
 fi
 
