7746085b8f999f28e44dfdf9752d986786c64b27
Chandler Cain <chandler@clcain.com>
2020-09-08 07:13:57 -0400

    Allow DISABLE_MULTI_ROOM and CLIENT_ONLY_MULTI_ROOM to coexist
    If both of these are set, only allow Airplay and Bluetooth.

--- bluetooth-audio/start.sh
+++ bluetooth-audio/start.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-#Check for incompatible multi room and client-only setting
-if [[ -n $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
-  echo “DISABLE_MULTI_ROOM and CLIENT_ONLY_MULTI_ROOM cannot be set simultaneously. Ignoring client-only mode.”
-fi
- 
-#Exit service if client-only mode is enabled 
-if [[ -z $DISABLE_MULTI_ROOM ]] && [[ $CLIENT_ONLY_MULTI_ROOM == "1" ]]; then
-  exit 0
-fi
-
 #Exit service if DISABLE_BLUETOOTH is enabled
 if [[ $DISABLE_BLUETOOTH == "1" ]]; then
   exit 0
